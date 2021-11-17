92a4809fc7e29daaa28c7dd1d1ab1bc431823983
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-17 12:51:51 +0100

    Refactored and modularised the padded buttons on the welcome screen

---
+++ flash_chat_app/lib/padded_color_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PaddedColorButton extends StatelessWidget {
+  const PaddedColorButton({
+    Key? key,
+    required this.onPressed,
+    required this.color,
+    required this.textChild,
+  }) : super(key: key);
+
+  final Function() onPressed;
+  final Color color;
+  final String textChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            textChild,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- flash_chat_app/lib/screens/welcome_screen.dart
+++ flash_chat_app/lib/screens/welcome_screen.dart
@@ -4,6 +4,8 @@ import 'package:flash_chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
+import '../padded_color_button.dart';
+
 class WelcomeScreen extends StatefulWidget {
 
   static const String id = 'welcome_screen';
@@ -79,43 +81,19 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            PaddedColorButton(
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              color: Colors.lightBlueAccent,
+              textChild: 'Log In',
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            PaddedColorButton(
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+              color: Colors.blueAccent,
+              textChild: 'Register',
             ),
           ],
         ),
