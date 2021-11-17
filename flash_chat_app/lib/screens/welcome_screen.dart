aa6893ce83a61b5de497a6627818d916b8fa955f
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-17 12:53:54 +0100

    Used the padded button class on the login and registration screens

--- flash_chat_app/lib/screens/welcome_screen.dart
+++ flash_chat_app/lib/screens/welcome_screen.dart
@@ -3,7 +3,6 @@ import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flash_chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-
 import '../padded_color_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
